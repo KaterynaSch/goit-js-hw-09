42b91925d011f180b0e293e9399005bbbd4cba19
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2023-08-14 16:13:45 +0300

    add-1-task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
+
+startBtn.addEventListener('click', onClickStart);
+stopBtn.addEventListener('click', onClickStop);
+let interval = 0;
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+function onClickStart() {
+    interval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+    }, 1000);
+};
+
+function onClickStop() {    
+    interval = clearInterval(interval);
+    startBtn.disabled = false;  
+    stopBtn.disabled = true;
+};
+
+
