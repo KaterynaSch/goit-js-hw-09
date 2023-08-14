8ee3283b4de73461391ecc6e2d940447d184cef4
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2023-08-14 13:14:30 +0000

    Deploying to gh-pages from @ KaterynaSch/goit-js-hw-09@42b91925d011f180b0e293e9399005bbbd4cba19 🚀

---
+++ 01-color-switcher.d26a122c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.body;t.addEventListener("click",(function(){d=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(){d=clearInterval(d),t.disabled=!1,e.disabled=!0}));var d=0;e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.d26a122c.js.map
