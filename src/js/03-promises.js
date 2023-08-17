e1bc25c55d38d005ab44d881a060646a43f30cc2
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2023-08-17 21:26:43 +0300

    add-task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,45 @@
-const delayInput = document.querySelector('input[name="delay"]');
-console.log(delayInput);
+import Notiflix from 'notiflix';
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+const form = document.querySelector('.form');
+const firstDelay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const createButton = document.querySelector('button[type="submit"]');
+    
+function createPromise(position, delay) {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const shouldResolve = Math.random() > 0.3;
+            if (shouldResolve) {
+                resolve({ position, delay });
+            } else {
+                reject({ position, delay });
+            }
+        }, delay);
+    });
+    return promise;
+};
+    
+createButton.addEventListener('click', handlerSubmit);
+
+function handlerSubmit(evt) {
+    evt.preventDefault();
+    for (let i = 0; i < amount.value; i += 1) {
+
+        const firstDelayNumber = Number(firstDelay.value);
+        const stepNumber = Number(step.value);
+        
+        createPromise(i + 1,( firstDelayNumber + i * stepNumber))
+        .then(({ position, delay }) => {
+            console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            return Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+            console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+            return Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+        
+    };
+    form.reset();
+}
+
